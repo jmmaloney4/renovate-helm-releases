6a08782711a5f473bc5a823d8e0423efaccae192
Jack Maloney <git@jmmaloney4.xyz>
2021-07-19 01:53:46 -0500

    Update renovate.py and test

--- renovate.py
+++ renovate.py
@@ -141,15 +141,7 @@ def cli(ctx, cluster_path, excluded_folders, debug, dry_run, tolerate_yaml_error
     helm_releases = {}
     for (file, doc) in helm_release_docs:
         helm_release_name = namespaced_name(doc["metadata"])
-        release_spec = doc.get("spec")
-        if not release_spec:
-            log.debug(f"Skipping '{helm_release_name}': No 'spec' in HelmRelease")
-            continue
-        chart_yaml = release_spec.get("chart")
-        if not chart_yaml:
-            log.debug(f"Skipping '{helm_release_name}': No 'spec.chart'")
-            continue
-        chart_spec = chart_yaml.get("spec")
+        chart_spec = doc.get("spec",{}).get("chart",{}).get("spec", {})
         if not chart_spec:
             log.debug(f"Skipping '{helm_release_name}': No 'spec.chart.spec'")
             continue
@@ -178,15 +170,7 @@ def cli(ctx, cluster_path, excluded_folders, debug, dry_run, tolerate_yaml_error
             log.debug(f"Skipping '{helm_release_name}': Could not determine repo")
             continue
         # Renovate can only update chart specs that contain a name and version,
-        release_spec = doc.get("spec")
-        if not release_spec:
-            log.debug(f"Skipping '{helm_release_name}': No 'spec' in HelmRelease")
-            continue
-        chart_yaml = release_spec.get("chart")
-        if not chart_yaml:
-            log.debug(f"Skipping '{helm_release_name}': No 'spec.chart'")
-            continue
-        chart_spec = chart_yaml.get("spec")
+        chart_spec = doc.get("spec",{}).get("chart",{}).get("spec", {})
         if not chart_spec:
             log.debug(f"Skipping '{helm_release_name}': No 'spec.chart.spec'")
             continue
